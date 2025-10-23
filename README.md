d02893d9d607358bcc609bee8c4bee2d004419b0
Cuc Dang <dvthuycuc@gmail.com>
2025-10-23 13:24:25 +0200

    a

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # teaching-data
+Repo is using for collecting data purposes
+Page for collecting data: https://archive.ics.uci.edu/
+
+https://disease.sh/docs/#/COVID-19%3A%20JHUCSSE/get_v3_covid_19_historical
+
+
+No history number of players => require run every day to get it
+
+
+https://steamspy.com/api.php
+
+# history of number players
+https://steamcharts.com/app/570/chart-data.json
+
+
+# current number of players:
+https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=570
+
+# game information
+https://store.steampowered.com/api/appdetails?appids=570
+
+
+https://disease.sh/docs/#/COVID-19%3A%20JHUCSSE/get_v3_covid_19_historical
+https://dbdiagram.io/d/68f774832e68d21b41824dde
+https://dbdiagram.io/d/68f774832e68d21b41824dde
---
+++ data_collection.R
@@ -0,0 +1,18 @@
+
+# geting data breast_cancer_wisconsin
+url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data"
+feature_names <- c("radius_mean", "texture_mean", "perimeter_mean", "area_mean", "smoothness_mean",
+                   "compactness_mean", "concavity_mean", "concave_points_mean", "symmetry_mean",
+                   "fractal_dimension_mean",
+                   "radius_se", "texture_se", "perimeter_se", "area_se", "smoothness_se",
+                   "compactness_se", "concavity_se", "concave_points_se", "symmetry_se",
+                   "fractal_dimension_se",
+                   "radius_worst", "texture_worst", "perimeter_worst", "area_worst", "smoothness_worst",
+                   "compactness_worst", "concavity_worst", "concave_points_worst", "symmetry_worst",
+                   "fractal_dimension_worst")
+col_names <- c("ID", "Diagnosis", feature_names)
+data <- read.csv(url(url), header = FALSE, col.names = col_names)
+
+head(data)
+dim(data)
+
